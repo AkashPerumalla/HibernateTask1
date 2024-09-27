a692342b655397fee514ffcc158cfa2db5bfc259
AkashPerumalla <19jj1a0436@cej.jntuh.ac.in>
2024-09-27 18:36:55 +0530

    Initial Hibernate Project Task1

---
+++ hibernate_task1/src/main/java/hibernate_task1/StudentCRUD.java
@@ -0,0 +1,76 @@
+package hibernate_task1;
+
+import java.util.List;
+
+import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
+import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
+import javax.persistence.Query;
+
+public class StudentCRUD {
+
+	public void saveStudent(Student student) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("akash");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		
+		student.setId(student.getId());
+		student.setName(student.getName());
+		student.setAddress(student.getAddress());
+		
+		entityTransaction.begin();
+		entityManager.persist(student);
+		entityTransaction.commit();
+
+	}
+	
+	public void updateStudent(int id,String name,String address) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("akash");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		
+		
+		Student student = entityManager.find(Student.class, id);
+		student.setName(name);
+		student.setAddress(address);
+		
+		entityTransaction.begin();
+		entityManager.merge(student);
+		entityTransaction.commit();
+	}
+	public void deleteStudent(int id) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("akash");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		EntityTransaction entityTransaction = entityManager.getTransaction();
+		
+		
+		Student student = entityManager.find(Student.class, id);
+		
+		entityTransaction.begin();
+		entityManager.remove(student);
+		entityTransaction.commit();
+	}
+	
+	public void getStudentById(int id) {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("akash");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		Student student = entityManager.find(Student.class, id);
+		
+		System.out.println(student);
+		
+	}
+	
+	public void getAllStudents() {
+		EntityManagerFactory entityManagerFactory = Persistence.createEntityManagerFactory("akash");
+		EntityManager entityManager = entityManagerFactory.createEntityManager();
+		
+		Query query = entityManager.createQuery("select s from Student s");
+		
+		List<Student> list = query.getResultList();
+		System.out.println(list);
+	}
+	
+
+}
---
+++ hibernate_task1/src/main/java/hibernate_task1/StudentMain.java
@@ -0,0 +1,98 @@
+package hibernate_task1;
+
+import java.util.Scanner;
+
+public class StudentMain {
+
+	public static void main(String[] args) {
+		Scanner scanner = new Scanner(System.in);
+		Student student = new Student();
+		StudentCRUD crud = new StudentCRUD();
+		boolean check = true;
+		
+		
+		do {
+			
+			System.out.println(" 1.save student \n 2.update student \n 3.delete student \n 4.get student by id \n 5.get all students \n 6.exit");
+			int choice = scanner.nextInt();
+			switch (choice) {
+			case 1: {
+				System.out.println("Enter the id");
+				int id = scanner.nextInt();
+				System.out.println("enter the name");
+				String name = scanner.next();
+				System.out.println("enter the address");
+				String address = scanner.next();
+				
+				student.setId(id);
+				student.setName(name);
+				student.setAddress(address);
+				
+				crud.saveStudent(student);
+			}
+				
+				break;
+				
+			case 2: {
+				System.out.println("Enter the id");
+				int id = scanner.nextInt();
+				System.out.println("enter the name");
+				String name = scanner.next();
+				System.out.println("enter the address");
+				String address = scanner.next();
+				
+				student.setId(id);
+				student.setName(name);
+				student.setAddress(address);
+				
+				crud.updateStudent(id, name, address);
+			}
+				
+				break;
+				
+			case 3: {
+				System.out.println("Enter the id");
+				int id = scanner.nextInt();
+				
+				student.setId(id);
+				
+				crud.deleteStudent(id);
+			}
+				
+				break;
+				
+			case 4: {
+				System.out.println("Enter the id");
+				int id = scanner.nextInt();
+				
+				student.setId(id);
+				
+				crud.getStudentById(id);
+			}
+				
+				break;
+			case 5: {
+				
+				crud.getAllStudents();
+			}
+				
+				break;
+				
+			case 6: {
+				System.exit(0);
+			}
+
+			default: {
+				System.out.println("Invalid choice!!! \n Please select correct choice...");
+			}
+				
+				break;
+			}
+			
+		} while (check);
+		
+		
+
+	}
+
+}
